76a6fa68299933514292872369ab8b243ee318c6
Darshana1206 <darshanaravindu9196@gmail.com>
2019-10-19 16:25:10 +0530

    Add c file

---
+++ finding max.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main()
+{
+	int n,arr[n];
+	int i;
+	
+	printf("Enter the size of array: ");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
+	{
+		printf("array[%d] = ",i);
+		scanf("%d",&arr[i]);
+	}
+	
+	printf("print the array:");
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+	
+	//Find the max number of array
+	int max;
+	max=arr[0];
+	for(i=0;i<n;i++)
+	{
+		
+		if(arr[i]>=max)
+		{
+			max=arr[i];
+		}
+		
+		
+	}
+	printf("\nThe maximum number of array is= %d",max);
+	
+}
